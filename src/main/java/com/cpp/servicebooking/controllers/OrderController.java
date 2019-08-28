a4c1332ea3d739271e6b88dafa97f5694e2bb641
yingyingz <yingyingz@cpp.edu>
2019-08-27 23:50:34 -0700

    second

--- src/main/java/com/cpp/servicebooking/controllers/OrderController.java
+++ src/main/java/com/cpp/servicebooking/controllers/OrderController.java
@@ -1,17 +1,16 @@
 package com.cpp.servicebooking.controllers;
 
 import com.cpp.servicebooking.models.OrderRequest;
-import com.cpp.servicebooking.repository.OrderRepo;
 import com.cpp.servicebooking.services.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-
-import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/user")
 public class OrderController {
 
+    @Autowired
     private OrderService orderService;
 
     @GetMapping("/orders")
@@ -23,21 +22,7 @@ public class OrderController {
     @PostMapping("/post")
     public void postOrder(OrderRequest orderRequest)
     {
-        OrderService.insert(orderRequest);
+        orderService.insert(orderRequest);
     }
 
-
-
- /*   @PostMapping("/role")
-    public ResponseEntity<?> saveRole(@Valid @RequestBody RoleRequest roleRequest, BindingResult result) {
-        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
-        if(errorMap != null) return errorMap;
-
-        Role role = new Role(roleRequest.getName());
-        Role newRole = roleService.saveRole(role);
-        return new ResponseEntity<Role>(newRole, HttpStatus.CREATED);
-    }
-
-
-  */
 }
