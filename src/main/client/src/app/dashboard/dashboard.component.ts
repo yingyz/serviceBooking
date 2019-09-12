1b56559a1c7aaf1163e67f91e0b769ee3c93fa6c
Henry1118 <henryliu111889@gmail.com>
2019-09-11 21:35:39 -0700

    debug

--- src/main/client/src/app/dashboard/dashboard.component.ts
+++ src/main/client/src/app/dashboard/dashboard.component.ts
@@ -1,19 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {UserModel} from "../auth/user.model";
 import {AuthService} from "../auth/auth.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, OnDestroy {
 
+  private userSub: Subscription;
   user: UserModel;
   constructor(private authService: AuthService) { }
 
   ngOnInit() {
-    this.user = this.authService.getUser();
+    this.userSub = this.authService.user
+      .subscribe(
+        (user : UserModel) => {
+          this.user = user;
+        }
+      );
   }
 
+  ngOnDestroy(): void {
+    this.userSub.unsubscribe();
+  }
 }
